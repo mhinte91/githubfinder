519f320ee9c89d4d6af2b9077e0340e0a8375764
Michael Hinte <michaelhinte@michaels-mbp-2.attlocal.net>
2019-10-14 07:02:49 -0500

    CircleSelector coded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import CircleSelector from './components/CircleSelector/CircleSelector';
+
+const choices = [0, 1, 2, 3];
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: 0
+    };
+  }
+
+  handleClick = i => this.setState({ selected: i });
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>UNIT 4 FINAL ASSESSMENT</header>
-        <main>YOUR TWO COMPONENTS WILL GO HERE</main>
+        <main>
+          <CircleSelector
+            choices={choices}
+            choiceButton={this.state.selected}
+            handleClick={this.handleClick}
+          />
+        </main>
       </div>
     );
   }

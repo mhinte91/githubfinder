ad71acfe0705a0640b71bf6635bcc20f80e0e53a
Michael Hinte <mhinte91@gmail.com>
2019-10-15 09:05:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
-import CircleSelector from './components/CircleSelector/CircleSelector';
-import Circles from './components/Circles/Circles';
+import React from 'react';
 
-const choices = [0, 1, 2, 3];
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selected: 0
-    };
-  }
-
-  handleClick = i => this.setState({ selected: i });
-
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>UNIT 4 FINAL ASSESSMENT</header>
-        <main>
-          <CircleSelector
-            choices={choices}
-            choiceButton={this.state.selected}
-            handleClick={this.handleClick}
-          />
-          <Circles choices={choices} choiceCircle={this.state.selected} />
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;

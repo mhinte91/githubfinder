8680142e8d1aa235b6876bbac211deddaf651d0a
Michael Hinte <mhinte91@gmail.com>
2019-10-15 09:42:16 -0500

    creates Navbar components and implements PropTypes

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  };
+
+  static propTyps = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;

714390bbd7201ede5f6f7e1e0c0c7f8b5a29002f
Michael Hinte <mhinte91@gmail.com>
2019-10-15 09:54:09 -0500

    getting started with component state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <nav className='navbar bg-primary'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
+      <nav className='App'>
+        <Navbar />
+        <UserItem />
       </nav>
     );
   }

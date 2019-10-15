455e4e31094eab73301ff40c42ac454ff1bf3b95
Michael Hinte <mhinte91@gmail.com>
2019-10-15 09:19:14 -0500

    clean up project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-const App = () => {
-  return <div></div>;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
